e5365ef07d1383f1d8845fc32f8a3331f5edaba1
MoWael45 <mowael450@gmail.com>
2023-03-16 17:20:08 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,14 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 1 (Error)
  */
+
 int main(void)
 {
-	ssize_t len;
-	int fd;
-	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	len = sizeof(*str);
-	fd = open("/dev/stderr", O_WRONLY);
-	write(fd, str, len);
-	close(fd);
-
+	fputs("and that piece of art is useful\" - Dora Kor
+par, 2015-10-19\n", stderr);
 	return (1);
 }
