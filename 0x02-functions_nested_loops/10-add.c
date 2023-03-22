08667cd371eff4c74348854dac811fd0a85db6cb
MoWael45 <mowael450@gmail.com>
2023-03-22 21:46:47 +0200

    10.a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Adds two values
+ * @n1: The first number
+ * @n2: The second number
+ * Return: Integer value
+ */
+int add(int n1, int n2)
+{
+int res;
+
+res = n1 + n2;
+return (res);
+}
