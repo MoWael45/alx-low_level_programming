e1fac434a149c40c483cc815f418fe158b876f12
MoWael45 <mowael450@gmail.com>
2023-03-29 04:04:55 +0200

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -11,6 +11,7 @@
  *
  *
  */
+
 void reset_to_98(int *n)
 {
 *n = 98;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@ int _strlen(char *s);
 
 void _puts(char *str);
 
-void print_rev(char *s)
+
 
 void rev_string(char *s);
 
@@ -20,4 +20,4 @@ void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
 
-int _atoi(char *s)
+int _atoi(char *s);
