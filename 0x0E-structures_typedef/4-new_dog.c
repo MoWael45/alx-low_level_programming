84655b0fa3d268e0618ace166f6b57c8a2ca5a63
MoWael45 <mowael450@gmail.com>
2023-04-18 04:33:18 +0200

    edit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -39,6 +39,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 }
 
 /**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+	return (c);
+}
+
+/**
  * *_strcpy - copies string to given memory location
  * @dest: where the string needs to be copied
  * @src: where the string is
